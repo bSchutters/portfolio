a1a9bf0e208c620e9f971bff2f5488c884ac1356
bSchutters <bryan021@hotmail.be>
2024-01-30 21:55:52 +0100

    depedencies install

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./main.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home/home";
+import NotFound from "./pages/notfound/notfound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
