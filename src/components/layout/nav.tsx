83a19fa41b2d31917c556ef7b3a1443c2f5ade92
bSchutters <bryan021@hotmail.be>
2024-01-30 23:37:51 +0100

    add: footer

--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -1,18 +1,22 @@
 import Card from "../common/card";
 import Link from "../common/link";
+import { LuMenu } from "react-icons/lu";
 
 export default function Nav() {
   return (
-    <nav className="flex justify-between text-xl">
+    <nav className="text-md flex justify-between">
       <Card className="text-lg font-bold tracking-wide">
-        <Link to="/">Schutters Bryan.</Link>
+        <Link to="/">bSchutters</Link>
       </Card>
-      <Card className="flex gap-5 font-medium tracking-wide">
+      <Card className="hidden gap-5 font-medium tracking-wide lg:flex">
         <Link to="/">Home.</Link>
         <Link to="/services">Services.</Link>
         <Link to="/about">About.</Link>
         <Link to="/work">Work.</Link>
         <Link to="/contact">Contact.</Link>
+      </Card>
+      <Card className="flex items-center justify-center lg:hidden">
+        <LuMenu size={22} />
       </Card>
     </nav>
   );
