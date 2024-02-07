f2452de9635c682defede3f60a95737d0b2cf9e8
bSchutters <bryan021@hotmail.be>
2024-02-07 16:27:09 +0100

    add: cursor + cursor hover work

--- src/components/common/work.tsx
+++ src/components/common/work.tsx
@@ -5,24 +5,36 @@ interface WorkProps {
   desc: string;
   img: string;
   to: string;
+  wip: boolean;
   className?: string;
 }
 
-export default function Work({ title, desc, img, to, className }: WorkProps) {
+export default function Work({
+  title,
+  desc,
+  img,
+  to,
+  className,
+  wip = true,
+}: WorkProps) {
+  const cursorAttributeName = wip ? "cursor-wip" : "cursor-open";
   return (
-    <Link
-      to={to}
-      className={`relative flex w-full flex-col gap-5 lg:w-4/5 ${className}`}
-    >
-      <img
-        src={img}
-        alt={title}
-        className="z-10 max-h-[450px] min-w-full rounded-3xl object-cover"
-      />
-      <div className="flex flex-col gap-1">
-        <div className="text-2xl font-bold text-white">{title}</div>
-        <div className="text-base font-bold text-secondary">{desc}</div>
-      </div>
-    </Link>
+    <>
+      <Link
+        to={to}
+        className={`work-card relative flex w-full flex-col gap-5 lg:w-4/5 ${className}`}
+      >
+        <img
+          src={img}
+          alt={title}
+          className="z-10 max-h-[450px] min-w-full rounded-3xl object-cover"
+          {...{ [cursorAttributeName]: "true" }}
+        />
+        <div className="flex flex-col gap-1">
+          <div className="text-2xl font-bold text-white">{title}</div>
+          <div className="text-base font-bold text-secondary">{desc}</div>
+        </div>
+      </Link>
+    </>
   );
 }
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,3 +1,4 @@
+import Cursor from "../common/cursor";
 import Footer from "./footer";
 import Nav from "./nav";
 
@@ -7,10 +8,13 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="flex min-h-screen max-w-full flex-col gap-24 bg-backg px-[50px] py-[25px] text-white lg:px-[100px] lg:py-[50px]">
-      <Nav />
-      {children}
-      <Footer />
-    </div>
+    <>
+      <Cursor />
+      <div className="flex min-h-screen max-w-full flex-col gap-24 bg-backg px-[50px] py-[25px] text-white lg:px-[100px] lg:py-[50px]">
+        <Nav />
+        {children}
+        <Footer />
+      </div>
+    </>
   );
 }
